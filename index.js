31e71ef2f6a6c50642113cf1d9a991cdab3d1fa0
Normandie-RP-LIFE-SEMI-WL <james2001del@gmail.com>
2026-05-01 19:42:02 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
+const { Client, GatewayIntentBits } = require('discord.js');
+const config = require('./config.json');
 
+const client = new Client({
+    intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages]
+});
+
+client.once('ready', () => {
+    console.log(`Connecté en tant que ${client.user.tag}`);
+});
+
+client.login(config.token);
